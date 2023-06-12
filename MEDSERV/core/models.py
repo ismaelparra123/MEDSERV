a24c957a59a164c6d07c85979fbc105ba971337f
Gonzalo Cabargas Contreras <gonzalo.cabargas.c@gmail.com>
2023-06-12 15:42:52 -0400

    models

--- MEDSERV/core/models.py
+++ MEDSERV/core/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Usuario(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100, verbose_name='nombre')
     email = models.EmailField()
     celular = models.CharField(max_length=12, verbose_name='fono')
-    
 
+    def __str__(self):
+        return self.nombre
+    
+class Producto(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100,verbose_name='nombre')
+    precio = models.IntegerField()
+    descripcion = models.TextField()
+    
     def __str__(self):
         return self.nombre
